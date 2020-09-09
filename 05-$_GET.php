26d294fa640d7fdaeefa54568c04dac7505a7472
Ariel <arielmello@rede.ulbra.br>
2020-09-09 14:21:07 -0300

    métodos get e post

---
+++ 05-$_GET.php
@@ -0,0 +1,8 @@
+<?php
+
+/*Toda vez que você acessa uma URL na web através de seu navegador você está realizando uma requisição 
+do tipo GET. */
+
+//Também poderia dizer que você está requisitando um arquivo, página, recurso, com o método GET.
+
+?>
