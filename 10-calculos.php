16332dacf6a0948f6d227f631be250acb961ed14
arielmell0 <arielmello@rede.ulbra.br>
2020-09-19 10:02:00 -0300

    switchecalc

---
+++ 10-calculos.php
@@ -0,0 +1,31 @@
+<?php
+
+// + adição
+// - subtração
+// * multiplicação
+// / divisão
+// % modulação (resto)
+
+$n1 = 7;
+$n2 = 3;
+
+//adição
+echo $n1 + $n2;
+echo '<br>';
+
+//subtração
+echo $n1 - $n2;
+echo '<br>';
+
+//multiplicação
+echo $n1 * $n2;
+echo '<br>';
+
+//divisão
+echo $n1 / $n2;
+echo '<br>';
+
+//resto de divisão (modulação)
+echo $n1 % $n2;
+
+?>
