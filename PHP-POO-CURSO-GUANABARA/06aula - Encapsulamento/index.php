b47c9d2cd3d03d3e864b39487665faf454829619
Ariel <arielmello@rede.ulbra.br>
2020-11-01 23:51:10 -0300

    aula 06

---
+++ PHP-POO-CURSO-GUANABARA/06aula - Encapsulamento/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 06 - Encapsulamento</title>
+</head>
+
+<body>
+    <h1>Projeto controle remoto</h1>
+    <pre>
+        <?php
+            require_once('ControleRemoto.php');
+
+            $c = new ControleRemoto();
+            $c -> ligar();
+            $c -> abrirMenu();
+
+        ?>
+    </pre>
+</body>
+
+</html>
